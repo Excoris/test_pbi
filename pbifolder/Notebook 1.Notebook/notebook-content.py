0f33baa1e1ce84b6721cb5666ae13e557f3b3d08
Excoris <protyayghosh28@gmail.com>
2025-03-12 19:54:40 +0000

    Committing 6 items from workspace e7726c0f-dd6e-4b36-9268-6d99914ed121

---
+++ pbifolder/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "7f949334-cbfc-4bac-8f10-eadb88827293",
+# META       "default_lakehouse_name": "lh_db_test",
+# META       "default_lakehouse_workspace_id": "e7726c0f-dd6e-4b36-9268-6d99914ed121",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "7f949334-cbfc-4bac-8f10-eadb88827293"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
